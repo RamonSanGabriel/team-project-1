f100a584259ea0043a3becb03e9f42d3339043a3
RamonSanGabriel <136789333+RamonSanGabriel@users.noreply.github.com>
2023-10-25 09:09:25 +0000

    Deploying to gh-pages from @ RamonSanGabriel/team-project-1@20657f3d8813c703124bfe800146d7974b6385d6 🚀

---
+++ index.f706ebcd.js
@@ -0,0 +1,2 @@
+var accordion=document.getElementsByClassName("container");for(i=0;i<accordion.length;i++)accordion[i].addEventListener("click",(function(){this.classList.toggle("active")}));
+//# sourceMappingURL=index.f706ebcd.js.map
