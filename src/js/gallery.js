e14c260639a6f092ada76fccda69433f7e41bf44
Karen <cafekarenclaire07@gmail.com>
2023-11-05 09:41:29 +0800

    New Changes

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,7 +1,5 @@
 function myFunction(imgs) {
   var expandImg = document.getElementById("expandedImg");
-  var imgText = document.getElementById("imgtext");
   expandImg.src = imgs.src;
-  imgText.innerHTML = imgs.alt;
   expandImg.parentElement.style.display = "inline-block";
 }
